2e2361aed82ebb2fc00390d5dadff96dafa6df86
Tyler Marshall <60017345+tymarshall1@users.noreply.github.com>
2023-09-11 15:15:09 -0400

    Read from the rs3Events.json file to set the events for each category

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
-fetch("rs3Events.json")
+let dailys = document.querySelector("#dailys");
+let weeklys = document.querySelector("#weeklys");
+let monthlys = document.querySelector("#monthlys");
+
+dailys.addEventListener("toggle", () => console.log("Placeholder for now"));
+
+const eventsJson = fetch("rs3Events.json")
   .then((response) => response.json())
-  .then((json) => console.log(json));
+  .then((json) => {
+    return json;
+  });
+
+let setAllEvents = async () => {
+  const eventObj = await eventsJson;
+  setEvent("dailys", eventObj);
+  setEvent("weeklys", eventObj);
+  setEvent("monthlys", eventObj);
+};
+
+function setEvent(eventType, eventObj) {
+  eventObj[eventType].map((event) => {
+    let singleEvent = document.createElement("div");
+    let eventName = document.createElement("h4");
+    let eventDisc = document.createElement("p");
+
+    singleEvent.classList.add("event");
+    eventName.textContent = event.name;
+    eventDisc.textContent = event.description;
+
+    singleEvent.appendChild(eventName);
+    singleEvent.appendChild(eventDisc);
+
+    determineEventTypeToSet(eventType, singleEvent);
+  });
+}
+
+function determineEventTypeToSet(eventType, singleEvent) {
+  if (eventType === "dailys") {
+    dailys.appendChild(singleEvent);
+  } else if (eventType === "weeklys") {
+    weeklys.appendChild(singleEvent);
+  } else if (eventType === "monthlys") {
+    monthlys.appendChild(singleEvent);
+  }
+}
+
+setAllEvents();
